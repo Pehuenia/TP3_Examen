3869e47e1c0fe10b5d7935022513b09dc4917ea1
ivan <ivangabrielk@hotmail.com>
2024-11-03 01:58:21 -0300

    hacemos el homeScreen

--- app/src/main/java/com/example/tp3_examen/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/tp3_examen/ui/screens/LoginScreen.kt
@@ -1,136 +1,160 @@
-package com.example.tp3_examen.ui.screens
+package com.example.tp3_examen.components1
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tp3_examen.R
-import com.example.tp3_examen.components1.ButtonApp
-import com.example.tp3_examen.components1.Input
 
 @Composable
-fun LoginScreen(){
-    var usuario by remember { mutableStateOf(TextFieldValue("")) }
-    var password by remember { mutableStateOf(TextFieldValue("")) }
+fun CreditCard(cardNumber: String, cardExpiryDate: String) {
+    var isRevealed by remember { mutableStateOf(false) }
 
     Box(
         modifier = Modifier
             .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
     ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(colorResource(id = R.color.green)),
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(top = (-100).dp) // Adjust this value to move the card upwards
         ) {
-            Column(
-                Modifier
-                    .align(Alignment.BottomCenter)
-                    .fillMaxWidth()
-                    .height(400.dp)
-                    .clip(RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp)) // Añade esto
-                    .border(0.dp, colorResource(id = R.color.gray_500), RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp)) // Añade el borde aquí
-                    .background(colorResource(id = R.color.gray_100)) // Mueve el background después de la propiedad clip
-                    .padding(horizontal = 12.dp, vertical = 24.dp),
-                verticalArrangement = Arrangement.spacedBy(24.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = stringResource(id = R.string.login),
-                    color = colorResource(id = R.color.black),
-                    style = TextStyle(
-                        fontSize = 16.sp,
-                        lineHeight = 22.4.sp,
-                        fontWeight = FontWeight.Bold
+            Card(
+                modifier = Modifier
+                    .width(336.dp)
+                    .height(212.dp)
+                    .shadow(
+                        6.dp,
+                        shape = androidx.compose.foundation.shape.RoundedCornerShape(
+                            8.dp,
+                            0.dp,
+                            0.dp,
+                            0.dp
+                        )
                     ),
-                    modifier = Modifier.fillMaxWidth(),
-                    textAlign = TextAlign.Start
-                )
-                Input(
-                    value = usuario,
-                    onValueChange = { usuario = it },
-                    label = "DNI o E-mail",
-                    errorMessage = "Formato de email invalido",
-                    isValid = { android.util.Patterns.EMAIL_ADDRESS.matcher(it).matches() },
-                )
-                Column(
-                    Modifier
-                        .fillMaxWidth(),
-                    verticalArrangement = Arrangement.spacedBy(12.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.BottomStart
                 ) {
-                    Input(
-                        value = password,
-                        onValueChange = { password = it },
-                        label = "Contraseña",
-                        errorMessage = "La contraseña debe tener al menos 4 caracteres",
-                        isValid = { it.length >= 4 },
-                        keyboardType = androidx.compose.ui.text.input.KeyboardType.Password,
-                        imeAction = androidx.compose.ui.text.input.ImeAction.Done,
-                        visualTransformation = androidx.compose.ui.text.input.PasswordVisualTransformation(),
-                        isPassword = true
+                    Image(
+                        painter = painterResource(id = R.drawable.bkg),
+                        contentDescription = "Background",
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop
                     )
+
+                    Image(
+                        painter = painterResource(id = R.drawable.bkg_1),
+                        contentDescription = "Triangle Bottom Left",
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop
+                    )
+
+                    Box(
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .offset(x = (-16).dp, y = (-24).dp)
+                            .size(55.dp, 34.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.vector),
+                            contentDescription = "Connector",
+                            modifier = Modifier
+                                .size(15.95.dp, 26.06.dp)
+                                .offset(19.26.dp, 3.97.dp)
+                        )
+
+                        Image(
+                            painter = painterResource(id = R.drawable.vector1),
+                            contentDescription = "Red Circle",
+                            modifier = Modifier
+                                .size(26.81.dp, 33.14.dp)
+                                .offset(0.43.dp, 0.43.dp)
+                        )
+
+                        Image(
+                            painter = painterResource(id = R.drawable.vector2),
+                            contentDescription = "Yellow Circle",
+                            modifier = Modifier
+                                .size(26.81.dp, 33.15.dp)
+                                .offset(27.24.dp, 0.42.dp)
+                        )
+                    }
+
+                    Image(
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = "Card Logo",
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .offset(x = (-16).dp, y = 16.dp)
+                            .size(50.dp, 28.09.dp)
+                    )
+
                     Text(
-                        text = stringResource(id = R.string.forgot_password),
-                        color = colorResource(id = R.color.purple_900),
+                        text = if (isRevealed) cardNumber else "${cardNumber.substring(0, 4)} **** ***** ${cardNumber.substring(15)}",
                         style = TextStyle(
-                            fontSize = 16.sp,
-                            lineHeight = 22.4.sp,
-                            fontWeight = FontWeight.Bold
+                            color = Color.White,
+                            fontSize = 20.sp,
+                            fontFamily = FontFamily.Serif,
                         ),
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-                                // Acción aquí
-                            },
-                        textAlign = TextAlign.End
+                            .align(Alignment.CenterStart)
+                            .offset(x = 16.dp, y = 40.dp)
+                    )
+
+                    Text(
+                        text = cardExpiryDate,
+                        style = TextStyle(
+                            color = Color.White,
+                            fontSize = 20.sp,
+                            fontFamily = FontFamily.Serif,
+                        ),
+                        modifier = Modifier
+                            .align(Alignment.CenterStart)
+                            .offset(x = 16.dp, y = 68.dp)
                     )
                 }
-                Spacer(modifier = Modifier.weight(1f))
-                Row(
-                    Modifier
-                        .fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = "Recordar datos de ingreso",
-                        color = colorResource(id = R.color.black),
-                        style = TextStyle(
-                            fontSize = 16.sp,
-                            lineHeight = 22.4.sp
-                        ),
-                        modifier = Modifier.padding(start = 8.dp)
-                    )
+            }
+
+            Row(
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .clickable { isRevealed = !isRevealed },
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                val eyeIcon = if (isRevealed) {
+                    painterResource(id = R.drawable.ojo_tachado)
+                } else {
+                    painterResource(id = R.drawable.ojo_abierto)
                 }
 
-                ButtonApp(text = "Ingresar", {})
+                Icon(
+                    painter = eyeIcon,
+                    contentDescription = "Eye Icon",
+                    modifier = Modifier.size(20.dp),
+                    tint = colorResource(id = R.color.purple_900)
+                )
+
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = if (isRevealed) "Ocultar datos" else "Mostrar datos",
+                    color = colorResource(id = R.color.purple_900),
+                )
             }
         }
     }
-
 }
