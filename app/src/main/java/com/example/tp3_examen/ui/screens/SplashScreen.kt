5e508676e1db2f25aeb465db509c7589926d0965
Heber Tescione <hebertescione@hotmail.com>
2024-11-04 15:47:58 -0300

    arreglo SplashScreen para que ocupe toda la pantalla

--- app/src/main/java/com/example/tp3_examen/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/example/tp3_examen/ui/screens/SplashScreen.kt
@@ -1,24 +1,16 @@
 package com.example.tp3_examen.ui.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.tp3_examen.R
 
 @Composable
-fun SplashScreen(){
+fun SplashScreen() {
     Splash()
 }
 
@@ -27,25 +19,13 @@ fun Splash() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-
     ) {
-        Column (
-            modifier = Modifier
-                .fillMaxSize()
-            ,horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ){
-            Image(
-                painter = painterResource(id = R.drawable.splash_screen),
-                contentDescription = "Fondo Splash",
-                Modifier.size(750.dp, 750.dp))
-        }
+        Image(
+            painter = painterResource(id = R.drawable.splash_screen),
+            contentDescription = "Fondo Splash",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
     }
-
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun SplashScreenPreview(){
-//    Splash()
-//}
