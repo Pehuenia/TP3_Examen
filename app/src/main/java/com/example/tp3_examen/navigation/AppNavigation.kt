3ebf3ae2a3a4e1458bc37c81ae51f3db08890bb5
Pehuenia <msaguero@gmail.com>
2024-11-07 15:35:53 -0300

    Se quitan importaciones en desuso

--- app/src/main/java/com/example/tp3_examen/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/tp3_examen/navigation/AppNavigation.kt
@@ -1,56 +1,23 @@
 package com.example.tp3_examen.navigation
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.tp3_examen.MainScaffold
-import com.example.tp3_examen.R
 import com.example.tp3_examen.components.drawermenu.CustomDrawer
-import com.example.tp3_examen.components.drawermenu.NavDrawer
-import com.example.tp3_examen.ui.screens.CargarSubeScreen
-import com.example.tp3_examen.ui.screens.HomeScreen
-import com.example.tp3_examen.ui.screens.MyCardScreen
-import com.example.tp3_examen.ui.screens.PagoDeServiciosScreen
-import com.example.tp3_examen.ui.screens.TransactionScreen
 import com.example.tp3_examen.viewmodels.LoginViewModel
 import com.example.tp3_examen.viewmodels.drawerviewmodel.DrawerViewModelFactory
 import com.example.tp3_examen.viewmodels.drawerviewmodel.NavDrawerViewModel
-import androidx.navigation.NavHostController
-
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
